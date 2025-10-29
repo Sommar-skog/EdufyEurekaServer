7190af9177df2ee8bb10b66d1784b712cd9a783f
Sommar-skog <amandaaronsson@hotmail.com>
2025-10-29 13:42:41 +0100

    Initial commit

---
+++ src/main/java/com/example/EdufyEurekaServer/EdufyEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.EdufyEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufyEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdufyEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EdufyEurekaServer/EdufyEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EdufyEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufyEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
